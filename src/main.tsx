2584d56616b00dbb9cd4edcc2672089d0f178e19
WinDY <71076716+windymaster009@users.noreply.github.com>
2026-07-15 15:25:09 +0700

    feat: use connected full-stack application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import AppWithBackend from './AppWithBackend'
 import './styles.css'
+import './backend.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppWithBackend />
   </StrictMode>,
 )
