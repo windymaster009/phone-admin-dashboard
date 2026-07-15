9800f931bcf9dcb50b14676c590085559b4ace8d
WinDY <71076716+windymaster009@users.noreply.github.com>
2026-07-15 18:08:56 +0700

    feat: mount operation modal bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppWithBackend from './AppWithBackend'
+import OperationModalBridge from './OperationModalBridge'
 import './styles.css'
 import './backend.css'
+import './operation-modals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppWithBackend />
+    <OperationModalBridge />
   </StrictMode>,
 )
