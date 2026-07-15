2a55aad3ce9d91b02f69c9a8e3a775ce0f6b0f9b
WinDY <71076716+windymaster009@users.noreply.github.com>
2026-07-15 18:32:56 +0700

    feat: mount live customer workspace

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppWithBackend from './AppWithBackend'
 import OperationModalBridge from './OperationModalBridge'
+import CustomerWorkspaceBridge from './CustomerWorkspaceBridge'
 import './styles.css'
 import './backend.css'
 import './operation-modals.css'
+import './customer-workspace.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppWithBackend />
     <OperationModalBridge />
+    <CustomerWorkspaceBridge />
   </StrictMode>,
 )
