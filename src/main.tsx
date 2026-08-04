858185270aabb61e9090b9494a35533b89bf65d6
WinDY <71076716+windymaster009@users.noreply.github.com>
2026-08-04 18:47:21 +0700

    perf: keep startup entrypoint minimal

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppWithBackend from './AppWithBackend'
-import DeferredBridges from './DeferredBridges'
 import './styles.css'
 import './backend.css'
 import './operation-modals.css'
@@ -11,6 +10,5 @@ import './activity-report.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppWithBackend />
-    <DeferredBridges />
   </StrictMode>,
 )
